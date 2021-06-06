617c56359f16cb5a631f945b94052812710e1736
bravo <luisgarcia.mec@outlook.com>
2021-06-06 14:42:35 -0500

    mod NotFound

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => (
     <div>
-        <h1>pagina no encontrada</h1>
+        <h1>pagina no encontrada regresa a la pagina anterior</h1>
     </div>
 );
 
